590bdd1154240e9abbe0257eea51594b209f6c4e
byodian <byj6696@gmail.com>
2021-01-05 09:28:10 -0500

    update index.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const { Composer } = require('micro-bot');
+
+const bot = new Composer();
+
+bot.start(ctx => {
+  
+  ctx.reply('Welcome');
+});
+
+module.exports = bot;
--- index.js
+++ index.js
@@ -1,10 +1,103 @@
-const { Composer } = require('micro-bot');
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const axios = require('axios');
+const express = require('express');
 
-const bot = new Composer();
+const app = express();
 
-bot.start(ctx => {
-  
-  ctx.reply('Welcome');
+// eslint-disable-next-line no-undef
+const NBA_KEY = process.env.NBA_KEY;
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const URL = process.env.URL || 'https://nbtelegrambot.herokuapp.com';
+
+const now = new Date();
+
+const getMonth= function(month) {
+  return month < 10 ? `0${month}` : `${month}`;
+}
+
+const getDate = function(date) {
+  return date < 10 ? `0${date}` : `${date}`;
+}
+
+const getCurrentDate = function() {
+  const year = now.getFullYear();
+  const month = now.getUTCMonth() + 1;
+  const date = now.getUTCDate();
+
+  return `${year}-${getMonth(month)}-${getDate(date)}`
+}
+
+// eslint-disable-next-line no-undef
+const bot = new Telegraf(BOT_TOKEN);
+bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
+app.use(bot.webhookCallback(`/bot${BOT_TOKEN}`));
+
+bot.on('sticker', (ctx) => ctx.reply('123'));
+bot.hears('hi', (ctx) => {
+  ctx.reply('Hey there');
 });
 
-module.exports = bot;
+bot.help((ctx) => ctx.reply('send me a sticker'));
+bot.command('today', (ctx) => {
+
+  const options = {
+    method: 'GET',
+    url: `https://api-nba-v1.p.rapidapi.com/games/date/${getCurrentDate()}`,
+    headers: {
+      'x-rapidapi-key': NBA_KEY,
+      'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'
+    }
+  };
+
+  axios.request(options).then((response) => {
+    let replyText = `<code>Visiting Team VS Home Team (Status)</code>\n\n`;
+
+    const { games } = response.data.api;
+
+    games.forEach(game => {
+      const { vTeam, hTeam } = game;
+      replyText += `<code>${vTeam.nickName} ${vTeam.score.points} - ${hTeam.score.points} ${hTeam.nickName}</code> <code>(${game.statusGame}) </code>\n\n`;
+    })
+
+    ctx.reply(replyText, {
+      parse_mode: 'HTML'
+    });
+  }).catch((error) => {
+    console.error(error);
+  });
+})
+
+bot.command('standings', (ctx) => {
+
+  const options = {
+    method: 'GET',
+    url: 'https://api-nba-v1.p.rapidapi.com/standings/standard/2020',
+    headers: {
+      'x-rapidapi-key': NBA_KEY,
+      'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'
+    }
+  };
+
+  axios.request(options).then(() => {
+    ctx.reply('敬请期待！');
+  }).catch((error) => {
+    console.error(error);
+  });
+})
+
+bot.command('live', (ctx) => {
+  ctx.reply('敬请期待！');
+})
+
+bot.command('players', ctx => {
+  ctx.reply('敬请期待！');
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)j;
+}
